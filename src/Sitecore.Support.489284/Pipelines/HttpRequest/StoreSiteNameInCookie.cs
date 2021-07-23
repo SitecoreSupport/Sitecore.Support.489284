6e9f2c6cbdafee705edd5f2fc8a0f8bda13862e1
artem-chunikhin <artem.chunikhin@sitecore.net>
2021-07-23 13:54:38 +0300

    Init

---
+++ src/Sitecore.Support.489284/Pipelines/HttpRequest/StoreSiteNameInCookie.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.XA.Foundation.Abstractions;
+using Sitecore.XA.Foundation.Multisite.Extensions;
+
+namespace Sitecore.XA.Foundation.Multisite.Pipelines.HttpRequest
+{
+    public class StoreSiteNameInCookie : HttpRequestProcessor
+    {
+        protected IContext Context { get; } = ServiceLocator.ServiceProvider.GetService<IContext>();
+
+        public override void Process(HttpRequestArgs args)
+        {
+            if (args.Context.Response.HeadersWritten)
+            {
+                return;
+            }
+            if (Context.Site != null && Context.Site.Name != "shell" && Context.Item != null)
+            {
+                var siteInfo = ServiceLocator.ServiceProvider.GetService<ISiteInfoResolver>().GetSiteInfo(Context.Item);
+                if (siteInfo != null)
+                {
+                    args.Context.Response.Cookies.Add(new HttpCookie("sxa_site", siteInfo.Name));
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.489284/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.489284")]
+[assembly: AssemblyProduct("Sitecore.Support.489284")]
+[assembly: ComVisible(false)]
